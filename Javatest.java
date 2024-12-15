ee2dc6b4ab548f29512b9f8a2e8f2ac9e193ea38
Danila <77702061+dkrgn@users.noreply.github.com>
2024-12-15 02:14:08 +0100

    java and js

---
+++ Javatest.java
@@ -0,0 +1,27 @@
+public class Javatest {
+
+    private String name;
+    private String email;
+
+    public Javatest(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+}
---
+++ jstest.js
@@ -0,0 +1,80 @@
+class TodoList {
+    constructor() {
+        this.tasks = [];
+    }
+
+    addTask(task) {
+        // Adds a new task to the to-do list
+        if (!this.tasks.includes(task)) {
+            this.tasks.push(task);
+            console.log(`Task added: '${task}'`);
+        } else {
+            console.log(`Task '${task}' already exists in the list.`);
+        }
+    }
+
+    removeTask(task) {
+        // Removes a task from the to-do list
+        const index = this.tasks.indexOf(task);
+        if (index !== -1) {
+            this.tasks.splice(index, 1);
+            console.log(`Task removed: '${task}'`);
+        } else {
+            console.log(`Task '${task}' not found in the list.`);
+        }
+    }
+
+    showTasks() {
+        // Displays all tasks in the to-do list
+        if (this.tasks.length > 0) {
+            console.log("To-Do List:");
+            this.tasks.forEach((task, index) => {
+                console.log(`${index + 1}. ${task}`);
+            });
+        } else {
+            console.log("No tasks in the to-do list.");
+        }
+    }
+
+    markDone(task) {
+        // Marks a task as done
+        const index = this.tasks.indexOf(task);
+        if (index !== -1) {
+            console.log(`Task '${task}' marked as done.`);
+            this.tasks.splice(index, 1);
+        } else {
+            console.log(`Task '${task}' not found.`);
+        }
+    }
+
+    getTaskCount() {
+        // Returns the number of tasks in the list
+        return this.tasks.length;
+    }
+}
+
+// Example usage
+const todo = new TodoList();
+
+// Add tasks to the list
+todo.addTask("Learn JavaScript");
+todo.addTask("Write blog post");
+todo.addTask("Go for a walk");
+
+// Show current tasks
+todo.showTasks();
+
+// Remove a task
+todo.removeTask("Write blog post");
+
+// Show tasks after removal
+todo.showTasks();
+
+// Mark a task as done
+todo.markDone("Go for a walk");
+
+// Show tasks after marking one as done
+todo.showTasks();
+
+// Display total number of tasks
+console.log("Total tasks:", todo.getTaskCount());
